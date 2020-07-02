fa74906a70a0eb23ec15fff096b3b0002f74bc3c
Sheila Eichenberger <grandpoobah@Sheilas-MacBook-Pro.local>
2020-07-02 11:35:18 -0700

    making corrections

--- stock_prices/stock_prices.py
+++ stock_prices/stock_prices.py
@@ -4,7 +4,20 @@ import argparse
 
 def find_max_profit(prices):
 
-  pass
+ # print(prices)
+  max_profit = min(prices) - max(prices)
+  if len(prices) < 2:
+    return max_profit
+
+  for s in range(1, len(prices)):
+    for b in range(len(prices[0:s])):
+      max_profit = max(max_profit, prices[s]-prices[b])
+      # print("b, s", b, s)
+      # print(f"prices[s]: {prices[s]}, prices[b]: {prices[b]}, max_profit: {max_profit}")
+
+  return max_profit
+
+  
   # 🔻below is from before hiatus
   # max_profit_so_far = 0
 
@@ -30,6 +43,33 @@ def find_max_profit(prices):
   # # prices[current_min_price_so_far_index]
   
   # return profit
+
+  '''
+  U:
+  inputs:
+    list of stock 'prices'
+  output:
+    maximum profit can be made
+  constraint:
+    max profit to be made from single buy and sell
+    must buy first before selling 
+      (has to be sequential, subtractor to gain)
+    profit can not be negative
+
+  P:
+  -buy+sell=positive max number ==> substract X[any index less than i] from X[i]
+  
+  for each i, 
+  set the cur_index = i
+
+  set `current_min_price_so_far_index` = cur_index
+ 
+  `max_profit_so_far` = X[i]
+  profit = `current` - `current_min_price_so_far`
+  
+  
+
+  '''
 
 
 if __name__ == '__main__':
